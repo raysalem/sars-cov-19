4695d6e8d5a171f62ec67e06e49fe4b1bb078888
ray salem <raysalem@gmail.com>
2020-04-08 09:17:06 -0700

    fix gui

--- compute.py
+++ compute.py
@@ -51,18 +51,27 @@ plt.rc('axes', prop_cycle=default_cycler)
 def getList(level,parent=""):
     if(level=="country"):
         idx    = pd.IndexSlice[:,:,:,:,"cases",level]
+        selIdx= 3;
+        f=lambda i: pd.IndexSlice[:,:,:,i,     "cases",level]
     elif(level=="state"):
         #must have country with state
         if(parent!=""):
             idx    = pd.IndexSlice[:,:,:,parent,"cases",level]
         else:
             idx    = pd.IndexSlice[:,:,:,:,     "cases",level]
-
-    g = dataTCS.loc[idx,:][dataTCS.loc[idx,"date"] == datetime.date(2020,4,1) ]
-    if(level=="country"):
-        return np.sort([c[3] for c  in g.index.values])
-    else:
-        return np.sort([c[2] for c  in g.index.values])
+        selIdx= 2;
+        f=lambda i: pd.IndexSlice[:,:,i,:,     "cases",level]
+    elif(level=="county"):
+        #must have country with state
+        if(parent!=""):
+            idx    = pd.IndexSlice[:,:,parent,:,"cases",level]
+        else:
+            idx    = pd.IndexSlice[:,:,:,:,     "cases",level]            
+        selIdx= 1;
+        f=lambda i: pd.IndexSlice[:,i,:,:,     "cases",level]
+    g = dataTCS.loc[idx,:][dataTCS.loc[idx,"date"] == (datetime.date.today()-datetime.timedelta(days=2)) ]
+    g.sort_values(by="value",inplace=True,ascending=False);      
+    return dict([ ["%s: %d"%(c[selIdx],dataTCS.loc[f(c[selIdx]),"value"].values[-1]),c[selIdx]] for c  in g.index.values])
 
 #** panda data frame styles
 from IPython.display import display_html
@@ -74,11 +83,12 @@ def display_side_by_side(*args):
     
     
     
-def plotter(minCount=5000,minCountKpi="cases",equalize=True,equalizeTrg="ITA",equalizeCount=100, plotKpi="deaths",logyPlot=True,
-           level="country", country="", reference=False, xlim=-1, ylim=-1):
+def plotter(minCount=5000,minCountKpi="cases",equalize=True,equalizeTrg="ITA",equalizeCount=100, 
+            plotKpi="deaths",logyPlot=True,
+            level="country", country="", reference=False, xlim=-1, ylim=-1, dteOffset=-2, maxEntries=12):
     idx = pd.IndexSlice
         
-    fig,ax = plt.subplots(1,1)
+    fig,ax = plt.subplots(1,1)    
     if(equalize):
         if(level=="country"):
             g = dataTCS.loc[idx[:,:,:,equalizeTrg,minCountKpi,level],:]
@@ -88,9 +98,9 @@ def plotter(minCount=5000,minCountKpi="cases",equalize=True,equalizeTrg="ITA",eq
             print("reduce equalize count")
             return
         gdts = g[g["value"]>equalizeCount]["date"].values[0]
-        gdte = g["date"].values[-1]
+        gdte = g["date"].values[dteOffset]
         N = (gdte-gdts).days#sp.size(g[g["value"]>equalizeCount]["date"].values)
-        print("Equalize to %s : date:%s to %s" % (equalizeTrg,gdts,gdte))
+        #print("Equalize to %s : date:%s to %s" % (equalizeTrg,gdts,gdte))
     if(reference):
         ref =  [8., 13., 21., 34.]
         for rate in np.array(ref)/100.:            
@@ -99,7 +109,7 @@ def plotter(minCount=5000,minCountKpi="cases",equalize=True,equalizeTrg="ITA",eq
             y0=equalizeCount
             M= sp.size(d.values)
             ax.plot(d,y0*np.exp(rate * np.arange(0,M)),'k--')#,label="ref%.1f"%(rate*100))
-        print(ref)
+        #print(ref)
     if(level=="country"):
         idxScreen = lambda ctr :idx[:,:,:,ctr,minCountKpi,level];
         idxPlot   = lambda ctr :idx[:,:,:,ctr,plotKpi    ,level];
@@ -109,30 +119,36 @@ def plotter(minCount=5000,minCountKpi="cases",equalize=True,equalizeTrg="ITA",eq
         idxScreen = lambda ctr :idx[:,:,ctr,:,minCountKpi,level];
         idxPlot   = lambda ctr :idx[:,:,ctr,:,plotKpi    ,level];
         # using states here
-        countries = np.sort(np.unique(dataTCS.loc[idx[:,:,:,country,"cases",level],:].index.values))[1:]
+        countries = np.sort(np.unique(dataTCS.loc[idx[:,:,:,country,"cases",level],:].index.values),ascending=False)
     
     # temp commented out    
+    entries=0
     for ctr in countries:  
         try:
-            if dataTCS.loc[idxScreen(ctr),"value"].values[-1]< minCount: continue     
+            if dataTCS.loc[idxScreen(ctr),"value"].values[dteOffset]< minCount: 
+                continue     
         except:
             continue
         try:
             g = dataTCS.loc[idxPlot(ctr),["date","value"]]        
         except:
-            continue
+            continue        
         if(equalize):
             #check if sufficent samples exist to equlize
-            if False == (g["value"]>equalizeCount).any():continue
+            if False == (g["value"][dteOffset]>equalizeCount).any():continue
+                
             dts = g[g["value"]>equalizeCount]["date"].values[0]
-            print("%s --> %d" %(ctr,(dts-gdts).days))
+            #print("%s --> %d" %(ctr,(dts-gdts).days))
             data= g[g["date"]>=dts]
             M = min(N,sp.size(data["value"]))
             ts = [(gdts + datetime.timedelta(i)) for i in range(0,sp.size(data["value"]))] 
         else:
             ts=g.index
-            data=g["value"]
-            M=sp.size(g)
+            data=g
+            M=sp.size(g["value"])
+            print(M)
+        if(entries>=maxEntries): continue
+        entries +=1
         ax.plot(ts[0:M],data["value"][0:M].values,label=dataTCS.loc[idxPlot(ctr),:].name.values[0][0:5])    
     
     ax.xaxis.set_major_locator(myLocator)
@@ -140,14 +156,14 @@ def plotter(minCount=5000,minCountKpi="cases",equalize=True,equalizeTrg="ITA",eq
     ax.grid()
     mm = max(dataTCS.loc[idx[:,:,:,:,plotKpi],"value"])
     if(ylim==-1):
-        ax.set_ylim(equalizeCount * .75, mm * 1.25)
+        ax.set_ylim(equalizeCount * .5, mm * 1.5)
     else:
-        ax.set_ylim(equalizeCount * .75, ylim)
+        ax.set_ylim(equalizeCount * .5, ylim)
     if(xlim==-1):
         ax.set_xlim(gdts,gdte)
     else:
         ax.set_xlim(gdts,gdts+datetime.timedelta(xlim))
-    ax.legend()
+    ax.legend(prop={'size': 6}, ncol=int(entries/2.))
     fig.autofmt_xdate()    
     plt.title("%s vs time"%plotKpi);
 
@@ -235,22 +251,24 @@ def logisModel(t,K,Q,B,v,M):
     #return A + (K-A) / ( (C + Q * np.exp(-B * (t-M)))**(1./v) )
     return K / ( (1+Q * np.exp(-B * (t-M)))**(1./v) )
 
-def plotty(ax1, ax2, y,v,yh, vh, C,minCount,label, label2=''):
-    M = np.argwhere(y>minCount)[0][0]
-    ax1.semilogy(yh)
-    ax1.semilogy(y)
+def plotty(ax1, ax2, y,v,yh, vh,logyPlot, label, label2='',):    
+    
+    
+    if(logyPlot) :
+        ax1.semilogy(yh)
+        ax1.semilogy(y)
+    else:
+        ax1.plot(yh)
+        ax1.plot(y)
     ax1.grid();
-    #print(np.max(np.max(yh),np.max(y)))
-    ax1.set_xlim([C-M,C+M])
-    ax1.set_ylim([minCount * .75,int(max(np.max(yh),np.max(y))* 1.25)])
+
     ax1.set_title(label)
     
     ax2.plot(vh)    
     ax2.plot(v)    
-    ax2.grid()    
-    ax2.set_xlim([C-M,C+M])
-    ax2.set_ylim([0,int(max(np.max(vh),np.max(v))* 1.25)])
+    ax2.grid()        
     ax2.set_title(label2)
+    
 
 
 
@@ -258,7 +276,11 @@ def projector(
     stype = 'cases',
     level = "state",
     target= "California",
-    minCount = 0
+    minCount = 100,
+    dteOffset=-2,
+    smoothLength=5,
+    smoothOrder =2,
+    logyPlot    =True
 ):
     """
     Project future growth
@@ -268,13 +290,15 @@ def projector(
     if(  level == "county"):  idx = pd.IndexSlice[:,    target,:,:, stype,level]
     elif(level == "state"):   idx = pd.IndexSlice[:,:,  target,:,   stype,level]
     elif(level == "country"): idx = pd.IndexSlice[:,:,:,target,     stype,level]
-    
-    y = dataTCS.loc[idx,"value"]
+    if(dteOffset==0):
+        y = dataTCS.loc[idx,"value"]
+    else:
+        y = dataTCS.loc[idx,"value"][0:dteOffset]
     if(sp.size(y)<5):
         print("not enough data")
         return
     y=y.astype("int32")
-    ys  = sp.signal.savgol_filter(y, 5, 2, mode='nearest')
+    ys  = sp.signal.savgol_filter(y, smoothLength, smoothOrder, mode='nearest')
     v=y.diff().values[1:]
     y=y[1:]
     A=max(v)
@@ -289,7 +313,7 @@ def projector(
 
     t = np.arange(0,sp.size(y))
     f = lambda t,B: newcModel(t, A, B, C)
-    paramsB, params_covariance = optimize.curve_fit(f, t, y, p0=[.2])
+    paramsB, params_covariance = optimize.curve_fit(f, t, y, maxfev  = int(1e4))
     B=paramsB[0]
 
     # did not work well,lowered value of B
@@ -297,46 +321,75 @@ def projector(
     paramsAB, params_covariance = optimize.curve_fit(f, t, y, p0=[A*1,.2],bounds=([A*0,-np.inf],[np.inf,np.inf]))
     Ae=paramsAB[0]
     Be=paramsAB[1]    
-    #print("%14s - %f:%f | %5d:%5d "%(ctr,B,Be, A,Ae))
 
-    # trying use function wiht more arguments
-    fLogistic = lambda t,K,B,Q,v: logisModel(t,K=K,Q=Q,B=B,v=v,M=C)
-    params, params_covariance = optimize.curve_fit(fLogistic,t,y,    bounds=([0,.02,0,0],[1e7,0.5,3,1]))  
-    params2, params_covariance2 = optimize.curve_fit(fLogistic,t,ys, bounds=([0,.02,0,0],[1e7,0.5,3,1]))  
+    # trying use function wiht more arguments, using original C
+    fLogistic = lambda t,K,B,Q,v,M: logisModel(t,K=K,Q=Q,B=B,v=v,M=np.clip(M,C-21,C+21))
+    params, params_covariance   = optimize.curve_fit(fLogistic,t,y,   maxfev  = int(1e4))  
+    params2, params_covariance2 = optimize.curve_fit(fLogistic,t,ys,  maxfev  = int(1e4))
+    print(params)
 
-    #M = 18# 2.5 weeks
-    #M = np.argwhere(yh>minCount)[0][0]
-    fig  = plt.figure(constrained_layout=True)
+    fig  = plt.figure()#constrained_layout=True)
     spec = fig.add_gridspec(3, 3)
-    ax00 = fig.add_subplot(spec[0, 0])
+    spec.update(wspace=0.0, hspace=0.0)
     ax10 = fig.add_subplot(spec[1, 0])
-    ax01 = fig.add_subplot(spec[0, 1])
-    ax11 = fig.add_subplot(spec[1, 1])
-    ax02 = fig.add_subplot(spec[0, 2])
-    ax12 = fig.add_subplot(spec[1, 2])
-    #ax2  = fig.add_subplot(spec[:,2])
+    ax00 = fig.add_subplot(spec[0, 0],sharex=ax10)    
+    ax11 = fig.add_subplot(spec[1, 1],sharey=ax10            );plt.setp(ax11.get_yticklabels(), visible=False)    
+    ax01 = fig.add_subplot(spec[0, 1],sharex=ax11,sharey=ax00);plt.setp(ax01.get_yticklabels(), visible=False)
+    ax12 = fig.add_subplot(spec[1, 2],            sharey=ax10);plt.setp(ax12.get_yticklabels(), visible=False)
+    ax02 = fig.add_subplot(spec[0, 2],sharex=ax12,sharey=ax00);plt.setp(ax02.get_yticklabels(), visible=False)
+    
 
-    t = np.arange(0,sp.size(y)+60)
+    
+    plt.setp(ax00.get_xticklabels(), visible=False)
+    plt.setp(ax01.get_xticklabels(), visible=False)
+    plt.setp(ax02.get_xticklabels(), visible=False)
+
+    t = np.arange(0,sp.size(y)+300)
 
     yh =newcModel(t,A,B,C)
     vh = cumModel(t,A,B,C)
-    plotty(ax00, ax10, y.values,v, yh,vh, C,minCount,"tanh","w/B=%.2f" % B)
+    plotty(ax00, ax10, y.values,v, yh,vh, label="tanh B=%.2f"%B,logyPlot=logyPlot)
 
 
-    yh = fLogistic(t,params[0],params[1],params[2],params[3])    
+    yh = fLogistic(t,params[0],params[1],params[2],params[3],params[4])    
+    yh1=yh
     vh = np.diff(yh)
-    plotty(ax01, ax11, y.values,v, yh,vh, C,minCount,"logistic")
+    vh1=vh
+    plotty(ax01, ax11, y.values,v, yh,vh, label="logistic",logyPlot=logyPlot)
 
-
-    yh = fLogistic(t,params2[0],params2[1],params2[2],params2[3])    
-    vh = np.diff(yh)
-    plotty(ax02, ax12, ys,vs,yh,vh, C,minCount,"logistic In:Smooth")
     Np = vh.argmax()    
-    NtoP=Np-sp.size(vs);
-    ax11.annotate("", xy=(sp.size(vs), vs[-1]), xytext=(Np, vs[-1]),
+    NtoP=Np-sp.size(v);
+
+    ax11.annotate("", xy=(sp.size(v), vh[-1]), xytext=(Np, vh[-1]),
                  arrowprops=dict(arrowstyle="<->", connectionstyle="arc3"))
     ax11.text(sp.size(vs), int(vs[-1]*.6),"%d"%(NtoP),  {'color': 'red', 'fontsize': 10})
+    
+    
+    yh = fLogistic(t,params2[0],params2[1],params2[2],params2[3],params2[4])    
+    yh2=yh
+    vh = np.diff(yh)      
+    vh2=vh
+    plotty(ax02, ax12, ys,vs,yh,vh,label="logistic In:Smooth",logyPlot=logyPlot)
+    
+    Np = vh.argmax()    
+    NtoP=Np-sp.size(vs);
 
+    ax12.annotate("", xy=(sp.size(vs), vh[-1]), xytext=(Np, vh[-1]),
+                 arrowprops=dict(arrowstyle="<->", connectionstyle="arc3"))
+    ax12.text(sp.size(vs), int(vs[-1]*.6),"%d"%(NtoP),  {'color': 'red', 'fontsize': 10})
+    
+    C=max(C, np.argmax(vh))
+    M = C-min(np.argwhere(yh1>minCount)[0][0],np.argwhere(yh2>minCount)[0][0])
+    
+    if(M <=1):
+        print("min count to close to peak")
+        return    
+    ax00.set_ylim([1+minCount * .5,int(max(np.max(yh1),np.max(yh2))* 1.5)])        
+    ax10.set_ylim([1,int(max(np.max(vh1),np.max(vh2))* 1.5)])   
+    
+    ax10.set_xlim([C-M,C+M])
+    ax11.set_xlim([C-M,C+M])
+    ax12.set_xlim([C-M,C+M])
 
     ss = dataTCS.loc[idx,"name"][0]#[i  for i in idx if i != '-1']
     print("{desc}: K={K:,} B={B:.2f} Q={Q:.2f} v={v:.2f} Ndays to peak=({N})".format(
@@ -345,3 +398,54 @@ def projector(
         ))       
 
  
+
+
+def getTimedGrowthRate(df,pidx,Nlength=3, debug=False,plot=False): 
+    yo = df.loc[pidx,:].sum().values
+    y  = sp.signal.savgol_filter(yo, 5, 2, mode='nearest')
+    if(plot):
+        plt.figure()
+        plt.plot(yo,label="original")
+        plt.plot(y, label="smoothed")
+        plt.legend()
+    
+     # daily new case
+    v = np.diff(y)#.values[1:]
+
+
+
+#     # find the start date 
+    idxs = np.argwhere(y>100)[2][0]    
+    dts  = df.loc[pidx,:].columns[idxs]
+    
+#     #find the inflection point (max of v)
+    idxi = sp.size(v)-1 #np.argmax(v)
+    dti  = df.loc[pidx,:].columns[idxi]
+    
+    if(debug):print("%d %d %d" %(idxs, idxi,sp.size(y)))
+    
+    vGR = np.zeros(idxi-idxs-(Nlength-1))       
+    i0= 0
+    
+    
+    for i in range(idxs,idxi-(Nlength-1)):
+        lv = sp.log10(y[i:i+Nlength])
+        #lv = y[i:i+Nlength]
+        if(debug):print(lv)
+        m=(lv[-1]-lv[0])/Nlength
+        if(debug):print("%d %.2f %.2f"%(i,m, np.exp(m)))
+        vGR[i0]=m             
+        i0+=1
+    if(plot):
+        fig = plt.figure()
+        ax1 = fig.add_subplot(111)
+        ax2 = ax1.twinx()
+        ax2.plot(vGR,'b+')
+        ax1.semilogy(y[idxs:idxi],'r')
+        ax1.semilogy(v[idxs:idxi],'g')
+    return vGR
+
+# country ="Germany"
+# pidx = pd.IndexSlice[country,"NA",:,:]
+# getTimedGrowthRate(dataJHU,pidx,plot=True,debug=False,Nlength=5);
+# dataJHU.loc[pidx].sum()
